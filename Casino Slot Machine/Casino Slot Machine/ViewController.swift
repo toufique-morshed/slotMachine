797097518c9d71ec337597200e55e14467fa8315
morshed <t.morshed1370@gmail.com>
2016-03-14 11:50:07 +0600

    just created the app

---
+++ Casino Slot Machine/Casino Slot Machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Casino Slot Machine
+//
+//  Created by Sekai Lab BD on 3/14/16.
+//  Copyright © 2016 Sekai Lab BD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
