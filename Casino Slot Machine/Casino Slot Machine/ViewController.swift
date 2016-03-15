7af0262deef2c44c86d3517328778a3bd24f1cb3
morshed <t.morshed1370@gmail.com>
2016-03-15 12:54:47 +0600

    level 2 completed

--- Casino Slot Machine/Casino Slot Machine/ViewController.swift
+++ Casino Slot Machine/Casino Slot Machine/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 
     //MARK: Properties
     var itemsToPicker:ItemsForPickerView?
+    var myTimer: NSTimer?
+    var numberOfIteration: Int?
     @IBOutlet weak var pickerViewLeft: UIPickerView!
     @IBOutlet weak var pickerViewMiddle: UIPickerView!
     @IBOutlet weak var pickerViewRight: UIPickerView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var spinButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,54 +46,109 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     //MARK: Button Behavior
     @IBAction func spinButtionAction(sender: UIButton) {
-        
-        for _ in 0...100 {
-        
-            var randomnumber = Int(arc4random_uniform(200))
-            var i  = pickerViewLeft.selectedRowInComponent(0)
-            print(i)
-            print(randomnumber)
-            if i < randomnumber {
-                while i <= randomnumber {
-                    pickerViewLeft.selectRow( ++i % 200, inComponent: 0, animated: true)
-                }
-            } else {
-                while i >= randomnumber {
-                    pickerViewLeft.selectRow( --i % 200, inComponent: 0, animated: true)
-                }
-            }
-        
-            randomnumber = Int(arc4random_uniform(200))
-            i  = pickerViewMiddle.selectedRowInComponent(0)
-            print(i)
-            print(randomnumber)
-            if i < randomnumber {
-                while i <= randomnumber {
-                    pickerViewMiddle.selectRow( ++i % 200, inComponent: 0, animated: true)
-                }
-            } else {
-                while i >= randomnumber {
-                    pickerViewMiddle.selectRow( --i % 200, inComponent: 0, animated: true)
-                }
-            }
-        
-            randomnumber = Int(arc4random_uniform(200))
-            i = pickerViewRight.selectedRowInComponent(0)
-            print(i)
-            print(randomnumber)
-            if i < randomnumber {
-                while i <= randomnumber {
-                    pickerViewRight.selectRow( ++i % 200, inComponent: 0, animated: true)
-                }
-            } else {
-                while i >= randomnumber {
-                    pickerViewRight.selectRow( --i % 200, inComponent: 0, animated: true)
-                }
-            }
-        }
-        
+        spinButton.hidden = true
+        numberOfIteration = 25
+        myTimer = NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(0.2), target: self, selector: "slotMachinePlay", userInfo: nil, repeats: true)
+
       
     }
+    
+    
+    func getRandomNumber(modValue: Int) -> Int {
+        var rndm = Int(arc4random_uniform(UInt32(modValue)))
+        while (rndm == 0 || rndm == modValue - 1) {
+            rndm = Int(arc4random_uniform(UInt32(modValue)))
+        }
+        return rndm
+    }
+    
+    
+    func spinPicker(pickerView: UIPickerView) {
+        var randomnumber = getRandomNumber(200)
+        pickerView.selectRow( randomnumber, inComponent: 0, animated: true)
+        
+    }
+    
+    
+    func slotMachinePlay()
+    {
+        numberOfIteration?--
+        spinPicker(pickerViewLeft)
+        spinPicker(pickerViewMiddle)
+        spinPicker(pickerViewRight)
+        if numberOfIteration == 0 {
+            myTimer?.invalidate()
+            let name1 = itemsToPicker!.namesOfImage[pickerViewLeft.selectedRowInComponent(0)]
+            
+            let name2 = itemsToPicker!.namesOfImage[pickerViewMiddle.selectedRowInComponent(0)]
+            
+            let name3 = itemsToPicker!.namesOfImage[pickerViewRight.selectedRowInComponent(0)]
+            
+            if name1 == name2 && name2 == name3 {
+                scoreLabel.text = "Winner"
+            } else {
+                scoreLabel.text = "Sorry...Try Again"
+            }
+            spinButton.hidden = false
+            
+        }
+        
+        
+        //var randomnumber = getRandomNumber(200)
+        
+        
+        //var i  = pickerViewLeft.selectedRowInComponent(0)
+        //pickerViewLeft.selectRow( randomnumber, inComponent: 0, animated: true)
+
+//        if i < randomnumber {
+//            while i <= randomnumber {
+//                pickerViewLeft.selectRow( ++i % 200, inComponent: 0, animated: true)
+//            }
+//        } else {
+//            while i >= randomnumber {
+//                pickerViewLeft.selectRow( --i % 200, inComponent: 0, animated: true)
+//            }
+//        }
+        
+        //randomnumber = getRandomNumber(200)
+        //pickerViewMiddle.selectRow( randomnumber, inComponent: 0, animated: true)
+        //i  = pickerViewMiddle.selectedRowInComponent(0)
+        //print(i)
+        //print(randomnumber)
+//        if i < randomnumber {
+//            while i <= randomnumber {
+//                pickerViewMiddle.selectRow( ++i % 200, inComponent: 0, animated: true)
+//            }
+//        } else {
+//            while i >= randomnumber {
+//                pickerViewMiddle.selectRow( --i % 200, inComponent: 0, animated: true)
+//            }
+//        }
+        
+        //randomnumber = Int(arc4random_uniform(200))
+        //pickerViewRight.selectRow( randomnumber, inComponent: 0, animated: true)
+//        while (randomnumber == 0 || randomnumber == 199) {
+//            randomnumber = Int(arc4random_uniform(200))
+//        }
+//        i = pickerViewRight.selectedRowInComponent(0)
+        //print(i)
+        //print(randomnumber)
+//        if i < randomnumber {
+//            while i <= randomnumber {
+//                pickerViewRight.selectRow( ++i % 200, inComponent: 0, animated: true)
+//            }
+//        } else {
+//            while i >= randomnumber {
+//                pickerViewRight.selectRow( --i % 200, inComponent: 0, animated: true)
+//            }
+//        }
+        
+        
+    }
+    
+    
+    
+    
     
     //MARK: Picker View Data Source
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
